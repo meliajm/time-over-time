dc843d806e1971e03544d24030281be84961e15d
github username <meliajm@gmail.com>
2020-04-07 09:21:10 -0700

    rendering functions to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
 let tasks = []
 
 const getFormInfo = () => document.querySelector('.add-task-form')
-// not sure if this is needed
-const getTaskList = () => document.querySelector('') 
+// not sure if this is needed below
+const getTaskList = () => document.querySelector('')
+
+const getTaskName = () => document.getElementById('').value 
+const getCategory = () => document.getElementById('').value
+const getContent = () => document.getElementById('').value
+
+function card(task) {
+    return `
+    <div class="card">
+        <div class="card-content">
+            <p>Content: ${task.content}</p>
+            <p>By when: ${task.by_when}</p>
+            <p>Category: ${task.category.name}</p>
+        </div>
+    </div>
+    `
+}
+
 function getTasks() {
     // fetch, sends a GET request by default
   
@@ -23,4 +40,20 @@ function getTasks() {
         renderTasks()
       })
       .catch(errors => console.log(errors));
-  }
+}
+
+function renderTasks() {
+    tasks.forEach(task => renderTask(task))
+}
+
+function renderTask(task) {
+    get???
+
+}
+
+function createNewTask(e) {
+
+    e.preventDefault()
+    
+}
+
