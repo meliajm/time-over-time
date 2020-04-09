2488df78d63a5acd2f184135daddcb2590d1659b
github username <meliajm@gmail.com>
2020-04-09 15:23:07 -0700

    validation for names of category in rails model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
     has_many :tasks
-    validates :name, presence: true#, uniqueness: true
+    # validates :name, presence: true#, uniqueness: true
+    validates :name, inclusion: { in: ['Physical Health', 'Mental Health', 'Emotional Health', 'Chores',
+                                        'Errands', 'Learning!', 'Relaxing', 'Working'] }
+
 end
